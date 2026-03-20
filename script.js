0c4e79d4a17f98bcff87ffade804fb3b72714504
unknown <mohandeveloper2020@gmail.com>
2026-03-20 18:12:39 +0530

    money denominator working app

---
+++ script.js
@@ -0,0 +1,11 @@
+const denominations = [500, 200, 100, 50, 20, 10, 5, 2, 1];
+
+const calculateAmount = (value, denomination) => {
+    document.getElementById('result' + denomination).textContent = denomination * value;
+
+    let total = 0;
+    denominations.forEach(denomination => {
+        total += parseInt(document.getElementById('result' + denomination).textContent);
+    });
+    document.getElementById('total').textContent = total;
+}
